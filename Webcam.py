3df22b3b41ce14b9627442ff63b57db8c2a6aa80
MarkCarsonDev <64717642+MarkCarsonDev@users.noreply.github.com>
2022-04-30 10:27:18 -0700

    Webcam changes

--- Webcam.py
+++ Webcam.py
@@ -1,12 +1,21 @@
-import cv2 as cv
+import cv2
 
-capture = cv.VideoCapture(0)
 
-while True:
-    isTrue,frame = capture.read()
-    cv.imshow('Video',frame)
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        cv2.imshow('FitYoShit', img)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
 
-capture.release()
-cv.destroyAllWindows()
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
