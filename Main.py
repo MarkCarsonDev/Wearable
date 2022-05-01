43f489cdcee2fc6036d00e25165f2be6d6dbc56c
MarkCarsonDev <64717642+MarkCarsonDev@users.noreply.github.com>
2022-04-30 20:27:55 -0700

    Final changes

--- Main.py
+++ Main.py
@@ -5,25 +5,27 @@ import numpy as np
 
 # Local classes
 import Webcam
-import RegionDetection 
+import RegionDetection
+
 
 def main():
+    # Start a camera using CV
     cam = cv.VideoCapture(0)
-    run = True
+    color = [0.4, 0.6, 0.4]
 
-    while (run):
+    while (True):
         im_cv = Webcam.get_image(cam, mirror=True)
         im_cv = cv.resize(im_cv, (0, 0), fx=0.5, fy=0.5)
 
         match = RegionDetection.getMatchBounds(im_cv, 'face')
-        
+
         x = match[0]
         y = match[1]
         w = match[2]
         h = match[3]
 
         cv.rectangle(im_cv, (x, y), (x+w, y+h), (255, 0, 255), 1)
-    
+
         sV = 1
 
         im_cv = cv.resize(im_cv, (0, 0), fx=sV, fy=sV)
@@ -36,68 +38,83 @@ def main():
 
         shirtCenter = int((2 * x + w) / 2), min(y + 2 * h, im_cv.shape[0])
 
-        shirtCorners = [(max(int(shirtCenter[0] - 2.5 * w), 0), max(y+h, 0)), (min(int(shirtCenter[0] + 2.5 * w), im_cv.shape[1]), min(shirtCenter[0] + 3 * h, im_cv.shape[0]))]
+        shirtCorners = [(max(int(shirtCenter[0] - 2.5 * w), 0), max(y+h, 0)), (min(int(
+            shirtCenter[0] + 2.5 * w), im_cv.shape[1]), min(shirtCenter[0] + 3 * h, im_cv.shape[0]))]
 
-        #cv.rectangle(im_cv, (shirtCenter[0]-1, shirtCenter[1]-1), (shirtCenter[0]+1, shirtCenter[1]+1), (255, 255, 0), )
         cv.rectangle(im_cv, shirtCorners[0], shirtCorners[1], (255, 255, 0), 1)
-    
-        # im_cv = cv.cvtColor(im_cv, cv.COLOR_BGR2RGB)
-        # im_pil = Image.fromarray(im_cv)
 
-        # ImageDraw.floodfill(im_pil, shirtCenter, color, thresh=80)
-
-        # im_np = np.asarray(im_pil)
-        # im_cv = cv.cvtColor(im_np, cv.COLOR_RGB2BGR)
-
-        
         k = cv.waitKey(30) & 0xff
-        if k==27:
-            run = False
+        if k == 27:
+            # ESC exits the program
             break
+        if k == 49:
+            color = [0, 0, 0]
+        if k == 50:
+            color = [0.3, 0.3, 0.3]
+        if k == 51:
+            color = [0.6, 0.6, 0.6]
+        if k == 52:
+            color = [1, 1, 1]
+        if k == 53:
+            color = [color[0] - 0.1, color[1], color[2]]
+        if k == 54:
+            color = [color[0] + 0.1, color[1], color[2]]
+        if k == 55:
+            color = [color[0], color[1] - 0.1, color[2]]
+        if k == 56:
+            color = [color[0], color[1] + 0.1, color[2]]
+        if k == 57:
+            color = [color[0], color[1], color[2] - 0.1]
+        if k == 58:
+            color = [color[0], color[1], color[2] + 0.1]
+        if color[0] > 1 or color[0] < 1:
+            color[0] = 0.1
+        if color[1] > 1 or color[1] < 1:
+            color[1] = 0.1
+        if color[2] > 1 or color[2] < 1:
+            color[2] = 0.1
 
         im_cv_hsv = cv.cvtColor(im_cv, cv.COLOR_BGR2HSV)
         shirtColor = im_cv_hsv[shirtCenter[1] - 1][shirtCenter[0]]
-        color = [0.3,0.7,1]
+
         shirtMask(im_cv, shirtColor, color, shirtCorners)
 
     cam.release()
     cv.destroyAllWindows()
+
 
 def shirtMask(im_cv, maskColor, color, corners):
 
     # Convert to HSV
     hsv = cv.cvtColor(im_cv, cv.COLOR_BGR2HSV)
 
-    
-
-    # Define lower and upper limits of what we call "white-ish"
+    # Color options
     sensitivity = 120
-    lower = np.array([0, 0, 10])
-    upper = np.array([255, 40, 245])
+    lower = np.array([0, 0, 20])
+    upper = np.array([255, 60, 245])
 
     # Create mask to only select white
     mask = cv.inRange(hsv, lower, upper)
 
-    # Draw new rectangular mask on old mask that is black inside the rectangle and white outside the rectangle
+    # Draw new rectangular mask for the shirt region
     mask2 = mask.copy()
     cv.rectangle(mask2, corners[0], corners[1], 0, -1)
 
-    # Change image to grey where we found white for combined mask
-    #result = im_cv.copy()
-    #result[mask2 > 0] = color
-
+    # Overlay masks and multiply
     a = np.where(mask > 0)
     ones = np.ones_like(im_cv)
+    color = [round(color[0], 2), round(color[1], 2), round(color[2], 2)]
     ones[a] = color
     result = im_cv*ones
 
-    cv.imshow('mask2', mask2 )
+    cv.imshow('mask2', mask2)
     cv.imshow('bruh', im_cv)
     cv.imshow('mask', mask)
 
     result = cv.resize(result, (0, 0), fx=1, fy=1)
     cv.imshow('result', result)
-    
+    print(color)
+
 
 if __name__ == "__main__":
     main()
