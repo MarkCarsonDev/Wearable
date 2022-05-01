40e011da957e4e1e53169f4b0740769550098e3e
CarlaZ488 <66750350+CarlaZ488@users.noreply.github.com>
2022-04-30 19:14:22 -0700

    tkinter with opencv

--- Webcam.py
+++ Webcam.py
@@ -1,19 +1,38 @@
+from tkinter import *
+import numpy as np
 import cv2
-import eel 
+from PIL import Image, ImageTk
 
-eel.init("website")
-@eel.expose
 def get_image(camera, mirror=False):
         ret_val, img = camera.read()
         if mirror: 
             img = cv2.flip(img, 1)
         return img
-eel.start("camera.html")
 
 if __name__ == '__main__':
     cam = cv2.VideoCapture(0)
     while (True):
-        cv2.imshow('FitYoShit', get_image(cam, mirror=True))
+
+
+        img = get_image(cam, mirror=True)
+
+        #Create an instance of tkinter frame
+        win = Tk()
+        win.geometry("700x550")
+        #Load the image
+        #img = cv2.imread('tutorialspoint.png')
+
+        #Rearrange colors
+        blue,green,red = cv2.split(img)
+        img = cv2.merge((red,green,blue))
+        im = Image.fromarray(img)
+        imgtk = ImageTk.PhotoImage(image=im)
+
+        #Create a Label to display the image
+        Label(win, image= imgtk).pack()
+        win.mainloop()
+
+
         if cv2.waitKey(1) == 27: 
             break  # esc to quit
         cv2.destroyAllWindows()
